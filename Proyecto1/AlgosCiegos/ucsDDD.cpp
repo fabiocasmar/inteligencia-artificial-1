d187c85e9352eda9bfdd2ee8ca91398cc9cb93ed
Fabio Castro <fabio@cesma.usb.ve>
2015-05-05 13:07:54 -0430

    arreglado ucs

--- Proyecto1/AlgosCiegos/ucsDDD.cpp
+++ Proyecto1/AlgosCiegos/ucsDDD.cpp
@@ -7,42 +7,50 @@
 using namespace std;
 
 struct orden{
-    bool operator()(nodo a, nodo b){
-        return a.get_costo() > b.get_costo();
+    bool operator()(nodo *a, nodo *b){
+        return a->costo > b->costo;
     }
 };
 
-void ucsDDD(state_t state){
-	priority_queue<nodo,vector<nodo >, orden> q;
+nodo* ucsDDD(state_t state){
+	priority_queue<nodo*,vector<nodo*>, orden> q;
 	int ruleid, costo;
     ruleid_iterator_t iter; 
 	state_map_t *mapa = new_state_map();
-
-	q.push(*(new nodo(state,state,0)));
+	nodo* nodoRaiz = new nodo(state,NULL,1);
+	q.push(nodoRaiz);
 
 	while (!(q.empty())) {
 	    state_t hijo;
-		nodo aux;
-		aux = q.top();
+		nodo* aux = q.top();
 		q.pop();
 
-		if (is_goal(aux.get_puntero())){
+		if (is_goal(&aux->puntero)){
 			cout << "Llegamos al goal! \n";
-			return;
+			return aux;
 		}
 
-		init_fwd_iter( &iter, aux.get_puntero() );
+		init_fwd_iter( &iter, &aux->puntero);
 	    while(( ruleid = next_ruleid( &iter ) ) >= 0 ) {
-	    	const int costo = aux.get_costo()+get_fwd_rule_cost(ruleid);
-	    	apply_fwd_rule(ruleid, aux.get_puntero(), &hijo);
+	    	const int costo = aux->costo+get_fwd_rule_cost(ruleid);
+	    	apply_fwd_rule(ruleid, &aux->puntero, &hijo);
 	    	const int *viejo_costo = state_map_get(mapa, &hijo);
             if (viejo_costo == NULL || *viejo_costo > costo ) {
+            	nodo *nodoAux = new nodo(hijo,aux,costo);
 				state_map_add(mapa, &hijo, costo);
-				q.push(*(new nodo(hijo,*aux.get_puntero(),costo)));
+				q.push(nodoAux);
 			}
 	    }
 	}
 	cout << "No hay camino hasta el goal \n";
+}
+
+void imprimirCamino(nodo* n){
+	if (n == NULL){
+		return;
+	}
+	cout << print_state(stdout,&n->puntero) << endl;
+	imprimirCamino(n->padre);
 }
 
 int main(){
@@ -59,5 +67,6 @@ int main(){
 		return 0; 
     }
 
-    ucsDDD(raiz);
+    nodo* salida = ucsDDD(raiz);
+    imprimirCamino(salida);
 }
